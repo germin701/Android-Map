5642715a3f7c2b1c08e221f9528fd7bac8dde5a2
germin701 <P20012293@student.newinti.edu.my>
2023-11-05 11:10:14 +0800

    update

--- app/src/main/java/com/example/geolocation/MainActivity.java
+++ app/src/main/java/com/example/geolocation/MainActivity.java
@@ -8,13 +8,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import android.util.Log;
 import android.content.Intent;
 import android.net.Uri;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private double doubleLat;
     private double doubleLong;
+    private Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,25 +31,12 @@ public class MainActivity extends AppCompatActivity {
 
         editText = findViewById(R.id.editTextTextPersonName);
         textView = findViewById(R.id.textView);
+        geocoder = new Geocoder(this, Locale.getDefault());
     }
 
     public void buttonGetCoordinates(View view){
         Geocoder geocoder = new Geocoder(this);
         List<Address> addressList;
-
-        /*try {
-            addressList = geocoder.getFromLocationName(editText.getText().toString(), 1);
-
-            if (addressList != null){
-                doubleLat = addressList.get(0).getLatitude();
-                doubleLong = addressList.get(0).getLongitude();
-
-                textView.setText("Latitude: " + String.valueOf(doubleLat) + " | " + "Longitude: " +
-                        String.valueOf(doubleLong));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
         try {
             addressList = geocoder.getFromLocationName(editText.getText().toString(), 1);
 
@@ -56,62 +44,57 @@ public class MainActivity extends AppCompatActivity {
                 doubleLat = addressList.get(0).getLatitude();
                 doubleLong = addressList.get(0).getLongitude();
 
-                textView.setText("Latitude: " + String.valueOf(doubleLat) + " | " + "Longitude: " +
-                        String.valueOf(doubleLong));
+                textView.setText("Latitude: " + doubleLat + " | " + "Longitude: " +
+                        doubleLong);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    /*private void openLocationInMaps(double latitude, double longitude, String label) {
-        Uri geoLocation = Uri.parse("geo:" + latitude + "," + longitude + "?q=" + Uri.encode(label));
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, geoLocation);
-
-        // Check if the Google Maps app is available and set the package name.
-        mapIntent.setPackage("com.google.android.apps.maps");
-
-        if (mapIntent.resolveActivity(getPackageManager()) != null) {
-            startActivity(mapIntent);
-        } else {
-            // Handle the case where the app is not installed or the action cannot be performed.
-            Log.d("tag", "cannot");
-        }
-    }
-
-    public void buttonGetCoordinates(View view){
-        Geocoder geocoder = new Geocoder(this);
-        List<Address> addressList;
-
-        try {
-            addressList = geocoder.getFromLocationName(editText.getText().toString(), 1);
-
-            if (addressList != null){
-                double doubleLat = addressList.get(0).getLatitude();
-                double doubleLong = addressList.get(0).getLongitude();
-
-                textView.setText("Latitude: " + String.valueOf(doubleLat)
-                        + " | " + "Longitude: " + String.valueOf(doubleLong));
-
-                // Open the location in Google Maps
-                openLocationInMaps(doubleLat, doubleLong, "Location Label");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
 
     public void openMap(View v) {
-        /*double Lat = 5.3416038;
-        double Long = 100.2792958;
-        String locationName = editText.getText().toString();*/
-        Log.i("tag", doubleLat + "," + doubleLong);
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        Uri uri = Uri.parse("geo:0,0?q=" + Uri.encode(editText.getText().toString()));
-        /*Uri uri = Uri.parse("geo:" + doubleLat + "," + doubleLong + "?q=" + Uri.encode("Marker Label"));
-        + "?q=" + Uri.encode("Marker Label")*/
-        intent.setData(uri);
-        Intent chooser = Intent.createChooser(intent, "Launch Maps");
-        startActivity(chooser);
+        String locationName = editText.getText().toString();
+
+        try {
+            List<Address> addresses = geocoder.getFromLocationName(locationName, 1);
+            if (addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+                doubleLat = address.getLatitude();
+                doubleLong = address.getLongitude();
+                Log.i("tag", doubleLat + "," + doubleLong);
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri uri = Uri.parse("geo:" + doubleLat + "," + doubleLong + "?q=" + Uri.encode(locationName));
+                intent.setData(uri);
+                Intent chooser = Intent.createChooser(intent, "Launch Maps");
+                startActivity(chooser);
+            } else {
+                // Handle the case where the location doesn't exist
+                textView.setText("Location not found. Please enter a valid location.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void validateLocation(View v) {
+        String locationName = editText.getText().toString();
+        Geocoder geocoder = new Geocoder(this);
+
+        try {
+            List<Address> addresses = geocoder.getFromLocationName(locationName, 1);
+
+            if (addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+                doubleLat = address.getLatitude();
+                doubleLong = address.getLongitude();
+                Log.i("tag", "Location exists: " + address.getFeatureName());
+            } else {
+                Log.i("tag", "Location does not exist.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.i("tag", "Error while validating location.");
+        }
     }
 }
